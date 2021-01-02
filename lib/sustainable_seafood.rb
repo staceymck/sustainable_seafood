ae7eace417b7d5bfa649b6ef7074af4fbf82049c
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-02 09:50:37 -0600

    Add class files to environment

--- lib/sustainable_seafood.rb
+++ lib/sustainable_seafood.rb
@@ -1,4 +1,13 @@
-require "sustainable_seafood/version"
+#originally listed/defaulted to: require "sustainable_seafood/version" 
+require_relative "./sustainable_seafood/version"
+
+#added ones below - order matters
+require_relative "./sustainable_seafood/fish"
+require_relative "./sustainable_seafood/fish_types/wild"
+require_relative "./sustainable_seafood/fish_types/farmed"
+require_relative "./sustainable_seafood/api"
+require_relative "./sustainable_seafood/cli" 
+#added section ends here
 
 module SustainableSeafood
   class Error < StandardError; end
