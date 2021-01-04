20855bd681b20210862e419cbe99d60ffdf0c10e
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-03 18:04:06 -0600

    Add attr_accessors and initialize method

--- lib/sustainable_seafood/fish.rb
+++ lib/sustainable_seafood/fish.rb
@@ -1,2 +1,11 @@
 class SustainableSeafood::Fish
+    attr_accessor :name, :aliases, :quote, :harvest_type
+
+    def initialize(fish_details)
+        self.name = fish_details["Species Name"]
+        self.aliases = fish_details["Species Aliases"]
+        self.quote = fish_details["Quote"]
+        self.harvest_type = fish_details["Harvest Type"]
+    end
+      
 end
