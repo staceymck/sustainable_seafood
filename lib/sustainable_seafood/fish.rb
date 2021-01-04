e401f181321ea5c7c76b34019ff2e101f15b57c7
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-03 18:50:31 -0600

    Create initialize, attr_accessors and way to save instances of class

--- lib/sustainable_seafood/fish.rb
+++ lib/sustainable_seafood/fish.rb
@@ -1,11 +1,17 @@
 class SustainableSeafood::Fish
     attr_accessor :name, :aliases, :quote, :harvest_type
+    @@all = []
 
-    def initialize(fish_details)
+    def initialize(fish_details) #fish_details represents a single fish element from the hash returned by the get_fish API class method
         self.name = fish_details["Species Name"]
         self.aliases = fish_details["Species Aliases"].scan(/(?<=">).+?(?=<)/)
         self.quote = fish_details["Quote"]
         self.harvest_type = fish_details["Harvest Type"]
+        @@all << self
+    end
+
+    def self.all #Would need custom self.all methods in the Wild and Farmed classes that select the wild and farmed fish from the Fish.all collection 
+        @@all
     end
       
 end
