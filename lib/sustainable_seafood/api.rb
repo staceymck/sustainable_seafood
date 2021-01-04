8f07b6bce196fb494f05835c70c89f487ba33e44
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 14:34:52 -0600

    Remove planning  notes

--- lib/sustainable_seafood/api.rb
+++ lib/sustainable_seafood/api.rb
@@ -7,7 +7,7 @@ class SustainableSeafood::API
         JSON.parse(response.body)
     end
 
-    def self.make_fish #when is the best time to call this? and where? Does it make sense to store it in the Fish class? 
+    def self.make_fish
         get_fish.each do |fish_details|
             if fish_details["Harvest Type"] == "Farmed"
                 SustainableSeafood::Farmed.new(fish_details)
@@ -17,20 +17,4 @@ class SustainableSeafood::API
         end
     end
 end
-
-    # Create a separate method for dividing the fish, or do it all in the make_fish method since
-    # this way requires instance variables? 
-    # def id_by_harvest_type
-    #     #separate wild and farmed fish within results
-    #     @wild = []
-    #     @farmed = []
-    #     get_fish.each do |fish|
-    #         if fish["Harvest Type"] == "Farmed"
-    #             farmed << fish
-    #         elsif fish["Harvest Type"] == "Wild"
-    #             wild << fish
-    #         end
-    #     end
-    # end
-
 
