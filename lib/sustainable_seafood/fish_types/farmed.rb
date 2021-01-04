6b7dd249facb0d3437ff5a114fa823ad8ed87b3a
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-03 20:33:06 -0600

    Add initialize method and self.all method

--- lib/sustainable_seafood/fish_types/farmed.rb
+++ lib/sustainable_seafood/fish_types/farmed.rb
@@ -1,3 +1,14 @@
 class SustainableSeafood::Farmed < SustainableSeafood::Fish
     attr_accessor :feeds, :env_impact, :farming_method
+
+    def initialize(fish_details)
+        super
+        self.feeds = fish_details["Feeds_"].strip
+        self.env_impact = fish_details["Environmental Considerations"] #will need to format this response
+        self.farming_method = fish_details["Farming Methods"].strip
+    end
+
+    def self.all
+        SustainableSeafood::Fish.all.select {|fish| fish.harvest_type == "Farmed"}
+    end
 end
