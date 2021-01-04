9b163262f2840315f9845e81e1667a96aa43e113
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 14:36:59 -0600

    Move super to bottom of initialize and use self as comparison tool for self.all method

--- lib/sustainable_seafood/fish_types/farmed.rb
+++ lib/sustainable_seafood/fish_types/farmed.rb
@@ -2,13 +2,13 @@ class SustainableSeafood::Farmed < SustainableSeafood::Fish
     attr_accessor :feeds, :env_impact, :farming_method
 
     def initialize(fish_details)
-        super
         self.feeds = fish_details["Feeds_"].strip
         self.env_impact = fish_details["Environmental Considerations"] #will need to format this response
         self.farming_method = fish_details["Farming Methods"].strip
+        super
     end
 
     def self.all
-        SustainableSeafood::Fish.all.select {|fish| fish.harvest_type == "Farmed"}
+        SustainableSeafood::Fish.all.select {|fish| fish.class == self}
     end
 end
