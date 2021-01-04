47d657065d4cf2aa8bf6c9e22fc1b4e30662f12e
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 14:37:50 -0600

    Move super to bottom of initialize and use self as comparison tool for self.all method

--- lib/sustainable_seafood/fish_types/wild.rb
+++ lib/sustainable_seafood/fish_types/wild.rb
@@ -2,14 +2,14 @@ class SustainableSeafood::Wild < SustainableSeafood::Fish
     attr_accessor :population, :habitat_impacts, :fishing_rate, :bycatch
 
     def initialize(fish_details)
-        super
         self.population = fish_details["Population"].strip
         self.habitat_impacts = fish_details["Habitat Impacts"].strip
         self.fishing_rate = fish_details["Fishing Rate"].strip
         self.bycatch = fish_details["Bycatch"].strip
+        super
     end
 
     def self.all #should I set up 'all' as a class instance variable on the Fish class or overwrite the reader like this?
-        SustainableSeafood::Fish.all.select {|fish| fish.harvest_type == "Wild"}
+        SustainableSeafood::Fish.all.select {|fish| fish.class == self}
     end
 end
