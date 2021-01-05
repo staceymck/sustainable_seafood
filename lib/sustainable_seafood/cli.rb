c81e211bb0b2b6146b07081ac7ffa60577e26154
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 22:25:50 -0600

    Refactor menu methods

--- lib/sustainable_seafood/cli.rb
+++ lib/sustainable_seafood/cli.rb
@@ -2,54 +2,57 @@ class SustainableSeafood::CLI
 
     def call
         puts "Welcome to Sustainable Seafood!"
-        puts "Access sustainability-related data for 100+ marine species"
-
-        while @input != "exit"
-            main_menu
-            get_input
-            #validate input
-        end
+        display_main_menu
+        main_menu_actions
     end
 
-    def main_menu
-        puts "Please select an option below. Enter the menu option's number to get started:
+    def display_main_menu
+        puts "To access sustainablity-related info for a specific marine species, 
+        enter the species name or select a number below to view species lists.
         1. See list of all 113 species
         2. See only farmed fish
         3. See only wild fish
-        4. Search for species by name
-
-        You can enter 'exit' at anytime to exit the program."
+        
+        Enter 'exit' at anytime to exit the program."
     end
 
-    def get_input
-        @input = gets.strip
+    def get_user_input
+        gets.strip
     end
 
-    def validate_input_main_menu
-        if @input.to_i.between?(1...4)
-
-            case @input
+    def main_menu_actions
+            input = get_user_input
+            case input
             when "1"
                 puts #sorted SustainableSeafood::Fish.all - listed by name
+                species_details_menu
             when "2"
                 puts #sorted SustainableSeafood::Farmed.all - listed by name
+                species_details_menu
             when "3"
-                puts #sorted SustainableSeafood::Wild.all - listed by name
-            when "4"
-                puts "Please enter the name of the species you'd like to look up:"
-                get_input 
-                #validate here or validate within the find_by_name_or_alias method?
-                find_by_name_or_alias(@input)
-            end 
+                puts  #sorted SustainableSeafood::Wild.all - listed by name
+                species_details_menu
+            when /exit/i
+                exit_program
+            else
+                # if find_by_name_or_alias(input)
+                #     #display fish details
+                # else
+                    invalid_input
+                    main_menu_actions
+                #end
+            end  
+    end
 
-        elsif @input.downcase == "exit"
-            exit_program
-        else
-            puts "Input not recognized. Please enter 1, 2, 3, 4 or exit."
-            get_input
-        end
+    def invalid_input
+        puts ""
+        puts "Input not recognized. Please enter a different selection:"
+    end
 
-
+    def species_details_menu
+        #collects user input and validates it against length of list
+        #if invalid, asks for input again and gives opportunity to return to main menu
+        #if valid, returns details about the fish
     end
 
     def exit_program #maybe clear the terminal screen using system "clear"
@@ -57,3 +60,4 @@ class SustainableSeafood::CLI
         exit
     end
 end
+
