43bd0fc43ed4e01c4643ee32de9a554d915237ef
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 21:41:40 -0600

    Change env_impact to env_considerations to better reflect content it labels

--- lib/sustainable_seafood/fish_types/farmed.rb
+++ lib/sustainable_seafood/fish_types/farmed.rb
@@ -1,9 +1,9 @@
 class SustainableSeafood::Farmed < SustainableSeafood::Fish
-    attr_accessor :feeds, :env_impact, :farming_method
+    attr_accessor :feeds, :env_considerations, :farming_method
 
     def initialize(fish_details)
         self.feeds = fish_details["Feeds_"].strip
-        self.env_impact = fish_details["Environmental Considerations"] #will need to format this response
+        self.env_considerations = fish_details["Environmental Considerations"] #will need to format this response
         self.farming_method = fish_details["Farming Methods"].strip
         super
     end
