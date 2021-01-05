f26c9831aafde73ef0c299af48f0ada803e5509a
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-05 15:18:20 -0600

    Add extra formatting to alias names

--- lib/sustainable_seafood/fish.rb
+++ lib/sustainable_seafood/fish.rb
@@ -4,7 +4,7 @@ class SustainableSeafood::Fish
 
     def initialize(fish_details) #fish_details represents a single fish element from the hash returned by the get_fish API class method
         self.name = fish_details["Species Name"]
-        self.aliases = fish_details["Species Aliases"].scan(/(?<=">).+?(?=<)/)
+        self.aliases = fish_details["Species Aliases"]
         self.quote = fish_details["Quote"]
         self.harvest_type = fish_details["Harvest Type"]
         @@all << self #if saving here to @@all, can't also save in Wild and Farmed to their own @@all variables because it creates duplicates
@@ -27,6 +27,11 @@ class SustainableSeafood::Fish
     def self.find_by_name_or_alias(fish_name)
         #validate the input 
         #return fish details
+    end
+
+    def aliases=(aliases)
+        alias_array = aliases.scan(/(?<=">).+?(?=<)/)
+        alias_array.map {|alias_name| alias_name.gsub("&#039;", "'")} 
     end
       
 end
