3c8bc9a9b9f7e17bfe3639abb0a6bfe8fd8c083f
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-04 22:23:51 -0600

    Stub out additional methods

--- lib/sustainable_seafood/fish.rb
+++ lib/sustainable_seafood/fish.rb
@@ -8,10 +8,17 @@ class SustainableSeafood::Fish
         self.quote = fish_details["Quote"]
         self.harvest_type = fish_details["Harvest Type"]
         @@all << self #if saving here to @@all, can't also save in Wild and Farmed to their own @@all variables because it creates duplicates
+        #why doesn't using self.class.all << self here allow the #all methods to work? They all return 0
     end
 
-    def self.all #Would need custom self.all methods in the Wild and Farmed classes that select the wild and farmed fish from the Fish.all collection 
+    def self.all #Would need custom self.all methods in the Wild and Farmed classes that select the wild and farmed fish objs from the Fish.all collection 
         @@all
     end
+
+    #sort method
+
+    #find_by_name_or_alias(fish_name)
+        #validate the input 
+        #return fish details
       
 end
