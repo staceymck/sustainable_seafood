4fcb6302e09c3a64c2ae363e5996c74daae6ea67
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-07 13:37:03 -0600

    Refactor format_lists method to shorten code line length

--- lib/sustainable_seafood/api.rb
+++ lib/sustainable_seafood/api.rb
@@ -9,8 +9,8 @@ class SustainableSeafood::API
 
     def self.make_fish
         get_fish.each do |fish_details|
-            ##### CLEANUP DATA ######
-            format_chars(fish_details) #has to come before format_aliases because alias method changes the alias string to an array
+
+            format_special_chars(fish_details) 
             format_aliases(fish_details)
             format_lists(fish_details)
 
@@ -22,20 +22,20 @@ class SustainableSeafood::API
         end
     end
 
-
-    ##### DATA FORMATTING METHODS ######
+    ##### DATA CLEANUP METHODS ######
     def self.format_aliases(fish_details)
         fish_details["Species Aliases"] = fish_details["Species Aliases"].scan(/(?<=">).+?(?=<)/)
     end
 
     def self.format_lists(fish_details)
-        if fish_details["Environmental Considerations"] != nil
-            fish_details["Environmental Considerations"] = fish_details["Environmental Considerations"].gsub(/\n|<\/li>|<\/?ul>|<\/?a[^>]*>/, "").gsub(/(?<=[^\A])<li>(?=[\w\s&]+:)/, "\n\n").gsub(/<li>/, "\n• ").gsub(/\A\n•\s/, "")
+        list = fish_details["Environmental Considerations"]
+        unless list == nil
+            list.gsub!(/\n|<\/li>|<\/?ul>|<\/?a[^>]*>/, "").gsub!(/(?<=[^\A])<li>(?=[\w\s&]+:)/, "\n\n")
+            list.gsub!(/<li>/, "\n• ").gsub!(/\A\n•\s/, "")
         end
     end
 
-    def self.format_chars(fish_details) #using 'fish_details[k]' = v.gsub... edits the original fish_details contents 
+    def self.format_special_chars(fish_details) 
         fish_details.each {|k, v| fish_details[k] = v.gsub("&#039;", "'").gsub("&amp;", "&").gsub("&nbsp;", "") if v.is_a? String}
     end
 end
-
