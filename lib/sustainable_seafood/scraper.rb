02667d17c0c548f9467eb3c55b06939e2614ca94
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-07 12:30:24 -0600

    Create Scraper class to pull mercury content

---
+++ lib/sustainable_seafood/scraper.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class SustainableSeafood::Scraper
+    URL = "https://www.fda.gov/food/consumers/advice-about-eating-fish"
+
+    def self.get_mercury_levels
+        @doc = Nokogiri::HTML(open(URL))
+
+        fish_table = @doc.css("#customTable tbody").first 
+        fish_table.css("tr").each do |row|
+            fish = row.css("td")[0].text
+            mercury = row.css("td")[1].text
+            if SustainableSeafood::Fish.find_by_name(fish)
+                SustainableSeafood::Fish.find_by_name(fish).mercury_levels = mercury
+            end
+        end
+    end
+end
