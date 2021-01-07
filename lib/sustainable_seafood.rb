49b5b22b0539fec762742b417dddce7b6c98c75b
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-07 12:29:39 -0600

    Require nokogiri

--- lib/sustainable_seafood.rb
+++ lib/sustainable_seafood.rb
@@ -7,11 +7,13 @@ require_relative "./sustainable_seafood/fish_types/wild"
 require_relative "./sustainable_seafood/fish_types/farmed"
 require_relative "./sustainable_seafood/api"
 require_relative "./sustainable_seafood/cli" 
+require_relative "./sustainable_seafood/scraper"
 require "net/http"
 require "open-uri"
 require "json"
 require "word_wrap"
 require "colorize"
+require 'nokogiri'
 #added section ends here
 
 module SustainableSeafood
